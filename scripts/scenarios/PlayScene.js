cd5c01e922a0ace29cdf2950503db1ceac272a09
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-11-22 12:17:49 -0300

    updates from develop

--- scripts/scenarios/PlayScene.js
+++ scripts/scenarios/PlayScene.js
@@ -1,23 +1,31 @@
+let coinLayer;
+let coins;
+let coinScore = 0;
+let tesourosEncontrados = 0;
+let text;
+let textObjetivo;
+let GameOver;
+
+
 class PlayScene extends Phaser.Scene {
     constructor(){
         super('PlayScene');
     }
      preload(){
-        this.load.tilemapTiledJSON('mapa', 'public/gameassets/newmap.json')
+        this.load.tilemapTiledJSON('mapa', 'gameassets/newmap.json')
         this.load.scenePlugin({
             key:'AnimatedTiles',
             url:'https://raw.githubusercontent.com/nkholski/phaser-animated-tiles/master/dist/AnimatedTiles.js',
             sceneKey:'animatedTiles',
             systemKey:'animatedTiles'
         });
+
+
+        this.canvas = this.sys.game.canvas;
      }
 
 
      create() {
-        let coinLayer;
-        let coins;
-        let coinScore = 1;
-        let text;
         this.sfxCoin = this.sound.add('coin')
         this.sfxDeath = this.sound.add('morte')
         this.sfxJump = this.sound.add('pulo')
@@ -33,8 +41,8 @@ class PlayScene extends Phaser.Scene {
         fundo.setCollisionByProperty({solido: true})
         solidos.setCollisionByProperty({solido: true})
         this.cameras.main.setBounds(0,0,mapa.widthInPixels, mapa.heightInPixels)
-        this.physics.world.setBounds( 0, 0, mapa.widthInPixels, mapa.heightInPixels + 100);
-        this.cameras.main.setZoom(2.0)
+        this.physics.world.setBounds( 0, 0, mapa.widthInPixels, mapa.heightInPixels);
+        this.cameras.main.setZoom(1.8)
         this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
         this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
         this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
@@ -45,7 +53,8 @@ class PlayScene extends Phaser.Scene {
         this.physics.add.collider(this.player, deadlys, hitDeadly, null, this);
         this.physics.add.overlap(this.player, coins, collectCoin, null, this);
 
-        this.cameras.main.startFollow(this.player)
+        this.teste = this.cameras.main.startFollow(this.player)
+
 
         coinLayer.forEach(object => {
             this.obj = coins.create(object.x, object.y, 'coins');
@@ -56,21 +65,36 @@ class PlayScene extends Phaser.Scene {
                this. obj.body.height = object.height; 
         });
 
+        text = this.add.text(this.cameras.main.x , this.cameras.main.y,
+            `Moedas: ${coinScore}`, {
+            fontSize: '20px',
+            fill: '#ffffff'
+          });
+
+        textObjetivo = this.add.text(this.cameras.main.x, this.cameras.main.y,
+            `Tesouros: ${tesourosEncontrados}/3`, {
+            fontSize: '20px',
+            fill: '#ffffff'
+          });
+
         function collectCoin(player, coin) {
-            this.sfxCoin.play()
+            this.sfxCoin.play();
             coin.destroy(coin.x, coin.y); 
-            coinScore = coinScore ++; 
+            coinScore = coinScore + 1; 
+            text.setText(`Moedas: ${coinScore}`)
             return false;
         }
 
 
+
         function hitDeadly (player, deadlys)
         {
-            this.sfxDeath.play();
+            this.sfxDeath.play()
+            coinScore = 0;
+            player.anims.play('death')
             player.body.enable = false
             this.scene.start('MenuScene');
         }
-
 
 
 
@@ -79,12 +103,17 @@ class PlayScene extends Phaser.Scene {
      update() {
         this.physics.world.setFPS(30);
         this.player.body.setVelocityX(0);
+        this.sfxJump = this.sound.add('pulo')
+
+        text.y = this.cameras.main.scrollY + 150
+        text.x = this.player.x  + 100
+        textObjetivo.y = this.cameras.main.scrollY + 180
+        textObjetivo.x = this.player.x  + 60
 
     
-
-        
         if(!Phaser.Geom.Rectangle.Overlaps(this.physics.world.bounds, this.player.getBounds())){
-            this.sfxDeath.play();
+            this.sfxDeath.play()
+            coinScore = 0
             this.gameOver = true;
             this.player.body.enable = false
             this.scene.start('MenuScene')
@@ -100,8 +129,8 @@ class PlayScene extends Phaser.Scene {
             this.player.flipX = false
         }
         if(this.w.isDown && this.player.body.onFloor()){
-            this.sfxJump.play();
             this.player.body.setVelocityY(-250)
+            this.sfxJump.play();
         }
 
         if((this.a.isDown || this.d.isDown) && this.player.body.onFloor()){
@@ -117,12 +146,6 @@ class PlayScene extends Phaser.Scene {
         }
         
 
-
-      /*  let cursors = this.input.keyboard.createCursorKeys();
-        if ((cursors.left.isDown || this.a.isDown) || (cursors.right.isDown || this.d.isDown)) this.player.setVelocityX(cursors.left.isDown || this.a.isDown ? -160 : 160);
-        else this.player.setVelocityX(0);
-        if ((cursors.up.isDown || this.w.isDown)) this.player.setVelocityY(cursors.up.isDown || this.w.isDown ? -100 : 100);
-        else this.player.setVelocityY(0);*/
     }
     
 
