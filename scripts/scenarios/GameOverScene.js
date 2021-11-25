5e5040ac3e009281c9640ba97dd5ff1f1664777c
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-11-25 14:53:42 -0300

    add sound

--- scripts/scenarios/GameOverScene.js
+++ scripts/scenarios/GameOverScene.js
@@ -8,10 +8,13 @@ class GameOverScene extends Phaser.Scene {
     this.load.image('logoGameOver', 'gameassets/GameOver.png');
     this.load.image('menu', 'gameassets/Voltarmenu.png');
     this.load.image('bg', 'gameassets/bg.png');
+    this.load.audio('morte', "gameassets/Death.wav");
 
 }
 
     create() {
+    this.sfxDeath = this.sound.add('morte')
+    this.sfxDeath.play();
     this.cursors = this.input.keyboard.createCursorKeys();
     this.add.image(0, 0, 'bg').setOrigin(0, 0).setScale(1.0);
     this.add.image(-100, 0, 'logoGameOver').setOrigin(0, 0).setScale(1.25);
--- scripts/scenarios/SucessScene.js
+++ scripts/scenarios/SucessScene.js
@@ -8,9 +8,12 @@ class SucessScene extends Phaser.Scene {
     this.load.image('sucess', 'gameassets/Sucess.png');
     this.load.image('menu', 'gameassets/Voltarmenu.png');
     this.load.image('bg', 'gameassets/bg.png');
+    this.load.audio('sucess', "gameassets/Sucess.wav");
 }
 
     create() {
+    this.sfxSucess = this.sound.add('sucess')
+    this.sfxSucess.play();
     this.cursors = this.input.keyboard.createCursorKeys();
     this.add.image(0, 0, 'bg').setOrigin(0, 0).setScale(1.0);
     this.add.image(-100, 0, 'sucess').setOrigin(0, 0).setScale(1.25);
